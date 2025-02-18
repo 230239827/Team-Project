c9ad24aa65b3956c3d9966f0264ce1019877e376
230239827 <230239827@aston.ac.uk>
2025-02-18 15:00:03 +0000

    Add files via upload

--- Group 2/Motor_control/main.c
+++ Group 2/Motor_control/main.c
@@ -11,24 +11,24 @@
 #include <util/delay.h>
 
 // Define motor control pins
-#define MOTOR1_PIN1 PB0  // Motor 1, Control Pin 1 (IN1)
-#define MOTOR1_PIN2 PB1  // Motor 1, Control Pin 2 (IN2)
-#define MOTOR2_PIN1 PB2  // Motor 2, Control Pin 1 (IN3)
-#define MOTOR2_PIN2 PB3  // Motor 2, Control Pin 2 (IN4)
-#define MOTOR3_PIN1 PB4 // Motor 3, Control Pin 1 (IN5)
-#define MOTOR3_PIN2 PB5 // Motor 3, Control Pin 2 (IN6)
-#define MOTOR4_PIN1 PB6 // Motor 4, Control Pin 1 (IN7)
-#define MOTOR4_PIN2 PB7 // Motor 4, Control Pin 2 (IN8)
+#define MOTOR1_PIN1 PA0  // Motor 1, Control Pin 1 (IN1)
+#define MOTOR1_PIN2 PA1  // Motor 1, Control Pin 2 (IN2)
+#define MOTOR2_PIN1 PA2  // Motor 2, Control Pin 1 (IN3)
+#define MOTOR2_PIN2 PA3  // Motor 2, Control Pin 2 (IN4)
+#define MOTOR3_PIN1 PB0 // Motor 3, Control Pin 1 (IN5)
+#define MOTOR3_PIN2 PB1 // Motor 3, Control Pin 2 (IN6)
+#define MOTOR4_PIN1 PB2 // Motor 4, Control Pin 1 (IN7)
+#define MOTOR4_PIN2 PB3 // Motor 4, Control Pin 2 (IN8)
 
 // Function to move forward
 void Move_Forward() {
 	// Motor 1: IN1 = HIGH, IN2 = LOW
-	PORTB |= (1 << MOTOR1_PIN1);
-	PORTB &= ~(1 << MOTOR1_PIN2);
+	PORTA |= (1 << MOTOR1_PIN1);
+	PORTA &= ~(1 << MOTOR1_PIN2);
 
 	// Motor 2: IN3 = HIGH, IN4 = LOW
-	PORTB |= (1 << MOTOR2_PIN1);
-	PORTB &= ~(1 << MOTOR2_PIN2);
+	PORTA |= (1 << MOTOR2_PIN1);
+	PORTA &= ~(1 << MOTOR2_PIN2);
 
 	// Motor 3: IN5 = HIGH, IN6 = LOW
 	PORTB |= (1 << MOTOR3_PIN1);
@@ -43,12 +43,12 @@ void Move_Forward() {
 // Function to move backward
 void Move_Backward() {
 	// Motor 1: IN1 = LOW, IN2 = HIGH
-	PORTB &= ~(1 << MOTOR1_PIN1);
-	PORTB |= (1 << MOTOR1_PIN2);
+	PORTA &= ~(1 << MOTOR1_PIN1);
+	PORTA |= (1 << MOTOR1_PIN2);
 
 	// Motor 2: IN3 = LOW, IN4 = HIGH
-	PORTB &= ~(1 << MOTOR2_PIN1);
-	PORTB |= (1 << MOTOR2_PIN2);
+	PORTA &= ~(1 << MOTOR2_PIN1);
+	PORTA |= (1 << MOTOR2_PIN2);
 
 	// Motor 3: IN5 = LOW, IN6 = HIGH
 	PORTB &= ~(1 << MOTOR3_PIN1);
@@ -62,44 +62,48 @@ void Move_Backward() {
 // Function to turn left
 void Turn_Left() {
 
-	PORTB |= (1 << MOTOR1_PIN1);
-	PORTB &= ~(1 << MOTOR1_PIN2);
+	PORTA |= (1 << MOTOR1_PIN1);
+	PORTA &= ~(1 << MOTOR1_PIN2);
 
-	PORTB |= (1 << MOTOR3_PIN1);
-	PORTB &= ~(1 << MOTOR3_PIN2);
+	PORTA |= (1 << MOTOR2_PIN1);
+	PORTA &= ~(1 << MOTOR2_PIN2);
 
 	// Right motors (Motor 2 and Motor 4): Stop
-	PORTB &= ~((1 << MOTOR2_PIN1) | (1 << MOTOR2_PIN2) | (1 << MOTOR4_PIN1) | (1 << MOTOR4_PIN2));
+	PORTB &= ~(1 << MOTOR4_PIN1) | (1 << MOTOR4_PIN2);
+	PORTB &= ~(1 << MOTOR3_PIN1) | (1 << MOTOR3_PIN2);
 
 }
 
 // Function to turn right
 void Turn_Right() {
 	// Right motors (Motor 2 and Motor 4): Move forward
-	PORTB |= (1 << MOTOR2_PIN1);
-	PORTB &= ~(1 << MOTOR2_PIN2);
+	PORTA |= (1 << MOTOR2_PIN1);
+	PORTA &= ~(1 << MOTOR2_PIN2);
 
 	PORTB |= (1 << MOTOR4_PIN1);
 	PORTB &= ~(1 << MOTOR4_PIN2);
 
 	// Left motors (Motor 1 and Motor 3): Stop
-	PORTB &= ~((1 << MOTOR1_PIN1) | (1 << MOTOR1_PIN2) | (1 << MOTOR3_PIN1) | (1 << MOTOR3_PIN2));
+	PORTB &= ~(1 << MOTOR1_PIN1) | (1 << MOTOR1_PIN2);
+	PORTB &= ~(1 << MOTOR3_PIN1) | (1 << MOTOR3_PIN2);
 }
 
 void Stop() {
-	PORTB &= ~((1 << MOTOR1_PIN1) | (1 << MOTOR1_PIN2) | (1 << MOTOR2_PIN1) | (1 << MOTOR2_PIN2) | (1 << MOTOR3_PIN1) | (1 << MOTOR3_PIN2) | (1 << MOTOR4_PIN1) | (1 << MOTOR4_PIN2));
+	 PORTA &= ~(1 << MOTOR1_PIN1) | (1 << MOTOR1_PIN2) | (1 << MOTOR2_PIN1) | (1 << MOTOR2_PIN2); 
+	 PORTB &= ~(1 << MOTOR3_PIN1) | (1 << MOTOR3_PIN2) | (1 << MOTOR4_PIN1) | (1 << MOTOR4_PIN2);
 }
 
 int main(void) {
 	// Set motor control pins as output
-	DDRB |= (1 << MOTOR1_PIN1) | (1 << MOTOR1_PIN2) | (1 << MOTOR2_PIN1) | (1 << MOTOR2_PIN2) | (1 << MOTOR3_PIN1) | (1 << MOTOR3_PIN2) | (1 << MOTOR4_PIN1) | (1 << MOTOR4_PIN2);
+	DDRA |= (1 << MOTOR1_PIN1) | (1 << MOTOR1_PIN2) | (1 << MOTOR2_PIN1) | (1 << MOTOR2_PIN2); 
+	DDRB |= (1 << MOTOR3_PIN1) | (1 << MOTOR3_PIN2) | (1 << MOTOR4_PIN1) | (1 << MOTOR4_PIN2);
 	// Stop all motors initially
 	Stop();
 
 	while (1) {
 		// Move forward for 2 seconds
 		Move_Forward();
-		_delay_ms(2000);
+		_delay_ms(1000);
 
 		// Stop for 1 second
 		Stop();
